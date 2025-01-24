fe9aee28b1ba2bbe804e1c031fdffa4a1a9de0cd
Felipe Alves Amodio <felipeaamodio@gmail.com>
2025-01-24 18:10:49 -0300

    using lib fast confetti

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef } from "react";
+import { StyleSheet, View, Button } from 'react-native';
+import { Confetti, ConfettiMethods } from "react-native-fast-confetti";
 
 export default function App() {
+  const confettiRef = useRef<ConfettiMethods>(null);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Confetti ref={confettiRef} autoplay={false} fadeOutOnEnd />
+
+      <Button title="Start" onPress={() => confettiRef.current?.restart()} />
+      <Button title="Pause" onPress={() => confettiRef.current?.pause()} />
+      <Button title="Play" onPress={() => confettiRef.current?.resume()} />
+      <Button title="Reset" onPress={() => confettiRef.current?.reset()} />
     </View>
   );
 }
@@ -16,5 +23,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 24
   },
 });
